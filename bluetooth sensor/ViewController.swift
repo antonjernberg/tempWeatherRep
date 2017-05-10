be2db038cd1d56f750f0c7b604466b81b1030543
Anton <anton@Antons-Mac.local>
2017-05-10 23:11:12 +0200

    Initial Commit

---
+++ bluetooth sensor/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  bluetooth sensor
+//
+//  Created by Anton on 2017-05-10.
+//  Copyright © 2017 Anton. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
